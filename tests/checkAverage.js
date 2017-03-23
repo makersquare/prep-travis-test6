6607d11d5a25a5768260eda37ec7ac953d7bc254
mmeigooni <mmeigooni@gmail.com>
2017-03-23 14:45:49 -0500

    adding another question

--- tests/checkAverage.js
+++ tests/checkAverage.js
@@ -1,3 +1,5 @@
+// checkAverage
+
 describe("average", function() {
   it("should return a number", function() {
     expect(typeof average(1, 12)).toBe("number");
@@ -9,4 +11,24 @@ describe("average", function() {
     console.log("expect average(2,4) to be 3");
   });
   console.log("expect average(2,4) to be 3 position 2");
+});
+
+// checkAge
+console.log('-------- checkAge tests --------')
+
+describe("checkAge", function() {
+  it("should return a string", function() {
+    expect(typeof(checkAge("Dan", "24"))).toBe("string");
+    console.log("input: typeof(checkAge('Dan', '24')), expected output: 'string'")
+  });
+  it("should welcome someone over 21", function() {
+    expect(checkAge("Dan", "24")).toBe("Welcome, Dan!");
+    console.log("checkAge('Dan', '24'), expected output: 'Welcome, Dan!'")
+  });
+  it("should welcome a 21 year old", function() {
+    expect(checkAge("Toni", "21")).toBe("Welcome, Toni!");
+  });
+  it("should bounce someone under 21", function() {
+    expect(checkAge("Rad", "4")).toBe("Go home, Rad!");
+  });
 });
